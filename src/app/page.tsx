b5f8e0549612402586e476388634248fbc16e60a
Kim Dong Hyeong <ehdgud502@naver.com>
2026-09-22 08:44:25 +0900

    Add scroll-driven work artwork

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import About from '@/components/About'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 import WaveBackdrop from '@/components/WaveBackdrop'
+import ScrollArtwork from '@/components/ScrollArtwork'
 import { getAllProjects } from '@/lib/projects'
 import { getSiteProfile } from '@/lib/profile'
 
@@ -22,6 +23,8 @@ export default function HomePage() {
 
         {/* About section */}
         <About profile={profile} />
+
+        <ScrollArtwork projects={projects} />
 
         {/* Projects grid with filter */}
         <ProjectsGrid projects={projects} />
